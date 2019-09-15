694aa2fada366167cef145646289afa2666e617a
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-09-14 20:49:01 -0600

    Use reduce to total an array of numbers

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let answer = document.querySelector("#answer")
+answer.innerText = "answer should show here"
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Code your solution herel;
+// let totalBatteries = 0;
+
+let totalBatteries = batteryBatches.reduce(function(total, batchNum){
+  return (batchNum + total)
+})
